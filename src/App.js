f0b4f22b966930787a56c94d6c6ff20283c54698
Lopez Delgado Jose Andres <isc17350296@gmail.com>
2022-06-14 12:06:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const persona = {
+
+  nombre: "Jose Andres",
+  apellidoPaterno: "Lopez",
+  apellidoMaterno: "Delgado",
+  edad: 23,
+  
+};
+
+
+const { nombre, apellidoPaterno, apellidoMaterno, edad} = persona;
+
+const contactoPersona = {
+  telefono: "551 700 9899",
+  email: "andreslopez@gmail.com",
+  empresa: "IMSS",
+}
+const nuevoTelefono = "555 522 7877";
+const infoPersona = {
+  ...persona, 
+  ...contactoPersona, 
+  puesto: "Trabajador",
+telefono: nuevoTelefono,
+ };
+
+//console.log(persona);
+//console.log(contactoPersona);
+//console.log(infoPersona); 
+
+
+const frutas = ["manzana", "naranja", "fresa", "pera"];
+const vegetales = ["ierva mora", "zanahoria", "espinaca"];
+const carnes = ["res", "cerdo", "pollo", "pescado"];
+
+const alimentos = [
+  ...frutas, 
+  ...vegetales, 
+  ...carnes,
+"otro alimento",
+ ];
+ 
+ console.table(frutas);
+ console.table(vegetales);
+ console.table(carnes);
+ console.table(alimentos);
+ 
+
+   return (
+  <div className="App">
+    <h3>
+  {`Soy ${nombre} ${apellidoPaterno} ${apellidoMaterno} y tengo ${edad} años.`}
+  </h3>
+ 
+  
+  
+  </div>
   );
 }
 
 export default App;
+
